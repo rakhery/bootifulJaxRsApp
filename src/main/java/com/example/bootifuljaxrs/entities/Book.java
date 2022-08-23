770bf883b82f10f7ecd093394cda5e46b1a3dba8
Christian RAKOTO <crakoto@yahoo.fr>
2022-08-23 14:02:12 +0200

    Configuration de la base ajout strategy=GeerationType.IDENTITY dans les deux entités

--- src/main/java/com/example/bootifuljaxrs/entities/Book.java
+++ src/main/java/com/example/bootifuljaxrs/entities/Book.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.util.Objects;
@@ -18,7 +19,7 @@ import java.util.Objects;
 @XmlRootElement(name = "book")
 public class Book {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String isbn;
--- src/main/java/com/example/bootifuljaxrs/entities/Customer.java
+++ src/main/java/com/example/bootifuljaxrs/entities/Customer.java
@@ -6,14 +6,35 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Objects;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 public class Customer {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Customer customer = (Customer) o;
+
+        if (!Objects.equals(id, customer.id)) return false;
+        return Objects.equals(name, customer.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
 }
