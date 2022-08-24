f8129bb9cc23e9523062288712dc71a2bd3558b1
Christian RAKOTO <crakoto@yahoo.fr>
2022-08-24 16:03:26 +0200

    Import de schema.sql et data.sql et verification, il ya 100 utilisateurs

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,1 +1,3 @@
 create table CUSTOMER(id int not null, name varchar(60));
+create table utilisateurs(id int not null auto_increment,lastname varchar(100) not null,
+firstname varchar(100) not null,email varchar(100) not null,password varchar(40) NOT NULL,role enum('normal','admin')not null, primary key (id))
